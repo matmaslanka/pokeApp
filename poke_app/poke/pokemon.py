4a0a537621775e663a86294c94d2a8926affa152
Mateusz <mates94@gmail.com>
2024-11-08 15:33:20 +0100

    New upgrades

--- poke_app/poke/pokemon.py
+++ poke_app/poke/pokemon.py
@@ -6,21 +6,61 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 cache.API_CACHE = os.path.join(BASE_DIR, 'cache', 'pokebase_cache')
 
+
 class Pokemon:
     def __init__(self, id):
         self.id = id
         self.name = pb.pokemon(id).name.title()
-        self.sprite_url = pb.SpriteResource('pokemon', id).url
+        self.sprite_url = pb.SpriteResource('pokemon', id, other=True, official_artwork=True).url
+        self.order = f'#{id:04}'
+        self.types = [pb.pokemon(id).types[i].type.name for i in range(len(pb.pokemon(id).types))]
+
+        weaknesses_all = [
+            pb.type_(pokemon_type).damage_relations.double_damage_from[i].name
+            for pokemon_type in self.types
+            for i in range(len(pb.type_(pokemon_type).damage_relations.double_damage_from))
+        ]
+
+        resistances = [
+            pb.type_(pokemon_type).damage_relations.half_damage_from[i].name
+            for pokemon_type in self.types
+            for i in range(len(pb.type_(pokemon_type).damage_relations.half_damage_from))]
+
+        self.weaknesses = set(weaknesses_all).difference(set(resistances))
+
+        self.sprite_type_url = []
+
+        for pokemon_type in self.types:
+            generation_viii = getattr(pb.type_(pokemon_type).sprites, 'generation-viii')
+            brilliant_diamond_and_shining_pearl = getattr(generation_viii, 'brilliant-diamond-and-shining-pearl')
+            self.sprite_type_url.append(brilliant_diamond_and_shining_pearl.name_icon)
+
+        self.sprite_weaknesses_url = []
+
+        for pokemon_weaknesses in self.weaknesses:
+            generation_viii = getattr(pb.type_(pokemon_weaknesses).sprites, 'generation-viii')
+            brilliant_diamond_and_shining_pearl = getattr(generation_viii, 'brilliant-diamond-and-shining-pearl')
+            self.sprite_weaknesses_url.append(brilliant_diamond_and_shining_pearl.name_icon)
+
+        self.hp = pb.pokemon(id).stats[0].base_stat
+        self.attack = pb.pokemon(id).stats[1].base_stat
+        self.defense = pb.pokemon(id).stats[2].base_stat
+        self.special_attack = pb.pokemon(id).stats[3].base_stat
+        self.special_defense = pb.pokemon(id).stats[4].base_stat
+        self.speed = pb.pokemon(id).stats[5].base_stat
 
     def __repr__(self):
         return f'{self.name} has an ID:{self.id}'
-#
-#
-# bulbasaur = Pokemon(1)
-#
-# print(bulbasaur)
-# pokemons = [{'name': pb.pokemon(i), 'url': pb.SpriteResource('pokemon', i).url} for i in range(1, 20)]
-#
-# pokemons = [Pokemon(i) for i in range(1, 20)]
-#
-# print(pokemons)
+
+
+bulba = Pokemon(1)
+bylbasa = {
+    'name': bulba.name,
+    'types': bulba.types,
+    'weaknesses': bulba.weaknesses,
+    'sprites': bulba.sprite_type_url,
+    'sprites_weaknesses': bulba.sprite_weaknesses_url,
+    'hp': bulba.hp
+}
+print(bylbasa)
+
--- poke_app/poke/static/poke/scripts/pokemonInfo.js
+++ poke_app/poke/static/poke/scripts/pokemonInfo.js
@@ -5,19 +5,81 @@ const pokemonItems = document.querySelectorAll('.pokedex-bar');
 pokemonItems.forEach(item => {
     item.addEventListener('click', () => {
         // Pobierz nazwę Pokemona z atrybutu data-pokemon
-        const pokemonName = item.getAttribute('data-pokemon');
+        const pokemonName = item.getAttribute('data-pokemon-name');
+        const pokemonOrder = item.getAttribute('data-pokemon-order');
         const pokemonImage = item.querySelector('img').src;
+        const pokemonHp = item.getAttribute('data-pokemon-hp');
+        const pokemonAttack = item.getAttribute('data-pokemon-attack');
+        const pokemonDefense = item.getAttribute('data-pokemon-defense');
+        const pokemonSpecialAttack = item.getAttribute('data-pokemon-special_attack');
+        const pokemonSpecialDefense = item.getAttribute('data-pokemon-special_defense');
+        const pokemonSpeed = item.getAttribute('data-pokemon-speed');
+
+        const pokemonTypeImage =item.getAttribute('data-pokemon-type-image');
+
+        const cleanPokemonTypeImage = pokemonTypeImage
+            .replace('[', '')        // Usuwamy znak '['
+            .replace(']', '')        // Usuwamy znak ']'
+            .replace(/'/g, '')       // Usuwamy wszystkie pojedyncze cudzysłowy
+            .split(',');             // Rozdzielamy po przecinkach
+
+        const pokemonWeaknessesImage =item.getAttribute('data-pokemon-weaknesses-image');
+
+        const cleanPokemonWeaknessesImage = pokemonWeaknessesImage
+                .replace('[', '')        // Usuwamy znak '['
+                .replace(']', '')        // Usuwamy znak ']'
+                .replace(/'/g, '')       // Usuwamy wszystkie pojedyncze cudzysłowy
+                .split(',');             // Rozdzielamy po przecinkach
 
         // Wyświetl informacje o Pokemonie w sekcji .pokemon-info
         document.getElementById('pokemon-name').textContent = pokemonName;
+        document.getElementById('pokemon-order').textContent = pokemonOrder;
         document.getElementById('pokemon-image').src = pokemonImage;
         document.getElementById('pokemon-image').alt = `${pokemonName} image`;
-    });
-});
+        document.getElementById('pokemon-type').textContent = 'Type';
+        document.getElementById('pokemon-weaknesses').textContent = 'Weaknesses';
+        document.getElementById('pokemon-hp-desc').textContent = 'hp: ';
+        document.getElementById('pokemon-hp').textContent = pokemonHp;
+        document.getElementById('pokemon-attack').textContent = pokemonAttack;
+        document.getElementById('pokemon-attack-desc').textContent = 'attack: ';
+        document.getElementById('pokemon-defense').textContent = pokemonDefense;
+        document.getElementById('pokemon-defense-desc').textContent = 'defense: ';
+        document.getElementById('pokemon-special_attack').textContent = pokemonSpecialAttack;
+        document.getElementById('pokemon-special_attack-desc').textContent = 'special_attack: ';
+        document.getElementById('pokemon-special_defense').textContent = pokemonSpecialDefense;
+        document.getElementById('pokemon-special_defense-desc').textContent = 'special_defense: ';
+        document.getElementById('pokemon-speed').textContent = pokemonSpeed;
+        document.getElementById('pokemon-speed-desc').textContent = 'speed: ';
 
-// Ustaw domyślne wartości po załadowaniu strony
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('pokemon-name').textContent = "Wybierz";
-    document.getElementById('pokemon-image').src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-    document.getElementById('pokemon-image').alt = "Pokemon image";
+        const pokemonTypeImageList = document.getElementById('pokemon-type-img-list');
+        pokemonTypeImageList.innerHTML = '';
+
+        cleanPokemonTypeImage.forEach(typeImg => {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+
+            img.src = typeImg.trim();
+            img.alt = 'Type image';
+
+            li.appendChild(img);
+            pokemonTypeImageList.appendChild(li);
+
+        });
+
+        const pokemonWeaknessesImageList = document.getElementById('pokemon-weaknesses-img-list');
+        pokemonWeaknessesImageList.innerHTML = '';
+
+        cleanPokemonWeaknessesImage.forEach(weaknessImg => {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+
+            img.src = weaknessImg.trim();
+            img.alt = 'Weaknesses image';
+
+            li.appendChild(img);
+            pokemonWeaknessesImageList.appendChild(li);
+
+        });
+
+    });
 });
